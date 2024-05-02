00ab0cc24d7c8f76e8df15c3c5d49b60c45b9d42
mnorthrip <132296449+mnorthrip@users.noreply.github.com>
2024-05-01 20:19:27 -0500

    Update All Mint Classics SQL queries.sql

--- All Mint Classics SQL queries.sql
+++ All Mint Classics SQL queries.sql
@@ -53,7 +53,8 @@ SELECT warehouseCode, productLine,
        COUNT(CASE WHEN inventoryStatus = 'Stocked' THEN 1 ELSE NULL END) AS 'stocked <5 yr',
        COUNT(CASE WHEN inventoryStatus = 'Overstocked' THEN 1 ELSE NULL END) AS 'overstocked <10 yr',
        COUNT(CASE WHEN inventoryStatus = 'Severely Overstocked' THEN 1 ELSE NULL END) AS 'severelyOverstocked >10 yr',
-       ROUND(COUNT(CASE WHEN inventoryStatus = 'Severely Overstocked' THEN 1 ELSE NULL END) / COUNT(1), 2) AS pctSeverelyOverstocked
+       ROUND(COUNT(CASE WHEN inventoryStatus = 'Severely Overstocked' THEN 1 ELSE NULL END) / COUNT(1), 2) 
+       AS pctSeverelyOverstocked
   FROM (SELECT warehouseCode, p.productCode, productName, productLine, SUM(quantityOrdered) AS totalOrders,
 	       ROUND((SUM(quantityOrdered) / (2 + (5/12))), 2) AS oneYearOrders, quantityInStock AS currentInventory, 
 	       ROUND(quantityInStock / (SUM(quantityOrdered) / (2 + (5/12))), 2) AS yearsInventoryLeft,
@@ -73,9 +74,10 @@ SELECT warehouseCode, productLine,
 
 -- Query 5
 -- Warehouse orders and inventory.
-SELECT pr.warehouseCode, warehouseName, COUNT(pr.productCode) AS products, orders AS totalOrders, ROUND((orders / (2 + (5/12))), 2) AS oneYearOrders,
-       ROUND((orders / (2 + (5/12))) * 5, 2) AS fiveYearsOrders, ROUND((orders / (2 + (5/12))) * 10, 2) AS tenYearsOrders,
-       SUM(quantityInStock) AS currentInventory, ROUND(SUM(quantityInStock) / (orders / (2 + (5/12))), 2) AS yearsInventoryLeft, 
+SELECT pr.warehouseCode, warehouseName, COUNT(pr.productCode) AS products, orders AS totalOrders, 
+       ROUND((orders / (2 + (5/12))), 2) AS oneYearOrders, ROUND((orders / (2 + (5/12))) * 5, 2) AS fiveYearsOrders, 
+       ROUND((orders / (2 + (5/12))) * 10, 2) AS tenYearsOrders, SUM(quantityInStock) AS currentInventory, 
+       ROUND(SUM(quantityInStock) / (orders / (2 + (5/12))), 2) AS yearsInventoryLeft, 
        ROUND(SUM(pr.quantityInStock) / (warehousePctCap / 100), 0) AS warehouseCap
   FROM products pr
   JOIN (SELECT p.warehouseCode, SUM(quantityordered) AS orders
