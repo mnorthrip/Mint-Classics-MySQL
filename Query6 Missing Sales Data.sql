3b1c8f4ed9e3bec0b242c886a6aea80c6a8a62eb
mnorthrip <132296449+mnorthrip@users.noreply.github.com>
2024-05-01 21:19:50 -0500

    Delete Query6 Missing Sales Data.sql

--- Query6 Missing Sales Data.sql
+++
@@ -1,6 +0,0 @@
--- Product missing sales data.
-SELECT warehouseCode, p.productCode, productName, productLine, quantityOrdered, quantityInStock
-  FROM orderdetails o
- RIGHT JOIN products p
-    ON o.productCode = p.productCode
- WHERE p.productCode = 'S18_3233' 
