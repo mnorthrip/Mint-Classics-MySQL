a2b8aa1e4a61599b95bef17b97592c3d0a74e007
mnorthrip <132296449+mnorthrip@users.noreply.github.com>
2024-10-27 15:47:01 -0500

    Update mint_reduction.sql

--- mint_reduction.sql
+++ mint_reduction.sql
@@ -1,6 +1,6 @@
 -- Product orders, revenue, and inventory.
 SELECT p.warehouseCode, p.productName, p.productLine, SUM(od.quantityOrdered) AS totalOrders,
-	   ROUND((SUM(od.quantityOrdered) / (2 + (5/12)))) AS avgYearOrders,  
+       ROUND((SUM(od.quantityOrdered) / (2 + (5/12)))) AS avgYearOrders,  
        ROUND((p.MSRP - p.buyPrice) * (SUM(od.quantityOrdered) / (2 + (5/12))), 2) AS avgYearRevenue, 
        p.quantityInStock AS inventory,
        ROUND(p.quantityInStock / (SUM(od.quantityOrdered) / (2 + (5/12))), 2) AS yearsInventoryLeft
@@ -12,8 +12,8 @@ SELECT p.warehouseCode, p.productName, p.productLine, SUM(od.quantityOrdered) AS
  
 -- Warehouse capacity.
 SELECT p.warehouseCode, w.warehouseName, 
-	   ROUND(SUM(p.quantityInStock) / (w.warehousePctCap / 100), 0) AS warehouseCap
+       ROUND(SUM(p.quantityInStock) / (w.warehousePctCap / 100), 0) AS warehouseCap
   FROM products p
   JOIN warehouses w
-	ON p.warehouseCode = w.warehouseCode
+    ON p.warehouseCode = w.warehouseCode
  GROUP BY p.warehouseCode;
